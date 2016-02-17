edc9c21edf46519afab8fddbad1163d06d5d5bca
Trent Hashimoto <Trent Hashimoto>
2016-02-17 13:06:48 -0800

    Changed .gitignore files

--- Cube_CommandBase/src/org/usfirst/frc/team3663/robot/Robot.java
+++ Cube_CommandBase/src/org/usfirst/frc/team3663/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
