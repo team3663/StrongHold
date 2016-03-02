43801fd40e55f1f9833d74a17e7c89490e61b1a2
RedDogFang <kingbros@msn.com>
2016-03-02 09:50:24 -0800

    updating apps

--- GameSimulator/src/GSMain.java
+++ GameSimulator/src/GSMain.java
@@ -1,10 +1,18 @@
-import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.networktables.*;
 
 public class GSMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+		NetworkTable table;
+		
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("10.36.63.20");//78");
+		table = NetworkTable.getTable("Dog-NT");
+		table.putBoolean("autoInitFindLeft: ",true);
 
 		Simulator sim = new Simulator();
 		sim.runSimulations();
